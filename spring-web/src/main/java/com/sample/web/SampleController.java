d6f0816f0a0e32f7cc698c1311f58e17fb632e82
bmahn <lrllzzr@gmail.com>
2019-07-02 10:45:13 +0900

    SampleController 수정

--- spring-web/src/main/java/com/sample/web/SampleController.java
+++ spring-web/src/main/java/com/sample/web/SampleController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/sample")
 public class SampleController {
 	
-	@RequestMapping("")
-	public String menu() {
-		return "";
+	@RequestMapping("/main.do")
+	public String sample() {
+		return "sample/main";
 	}
 }
